20438ccc23b74f76a4c531f1ed457ab376847c2f
sd880428 <sd660428@gmail.com>
2022-12-07 14:58:14 +0800

    優化 命名風格

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const restaurant = require('./restaurant.json')
+const restaurants = require('./restaurant.json')
 const app = express()
 const port = 3000
 
@@ -9,20 +9,20 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => { //首頁
-  res.render('index', { restaurant: restaurant.results })
+  res.render('index', { restaurants: restaurants.results })
 })
 
 app.get('/restaurants/:restaurant', (req, res) => { //各餐廳路由
-  const target_restaurant = restaurant.results.find(restaurant => restaurant.id.toString() === req.params.restaurant)
-  res.render('show', { restaurant: target_restaurant })
+  const targetRestaurant = restaurants.results.find(restaurant => restaurant.id.toString() === req.params.restaurant)
+  res.render('show', { restaurant: targetRestaurant })
 })
 
 app.get('/search', (req, res) => { //搜尋
   const keyword = req.query.keyword.toLowerCase()
-  const filter_restaurant = restaurant.results.filter(restaurant => {
+  const filterRestaurants = restaurants.results.filter(restaurant => {
     return `${restaurant.name.toLowerCase() + restaurant.category}`.includes(keyword)
   })
-  res.render('index', { restaurant: filter_restaurant, keyword: keyword })
+  res.render('index', { restaurants: filterRestaurants, keyword: keyword })
 })
 
 
