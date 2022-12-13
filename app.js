8bb9e33280928d70a776cd0652cf21072a8e0e81
sd880428 <sd660428@gmail.com>
2022-12-14 02:40:12 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars') 
 const restaurants = require('./restaurant.json')
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') { //僅在非正式環境時, 載入 dotenv
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true } ) //連線到mongoose資料庫
+
 const app = express()
 const port = 3000
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => { // 連線異常
+  console.log('mongodb error!')
+})
+
+db.once('open', () => { // 連線成功
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
