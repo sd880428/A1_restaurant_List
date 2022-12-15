ea377b43bed02600ea3f47cf80f2784cbc9a89a3
sd880428 <sd660428@gmail.com>
2022-12-16 01:12:17 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
