e9789906849f55351b1eeeb1fbe4aa29898b016a
sd880428 <sd660428@gmail.com>
2022-12-16 01:06:05 +0800

    modify mongoose config

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ require('./config/mongooes')
 
 const app = express()
 const port = 3000
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => { // 連線異常
+  console.log('mongodb error!')
+})
+
+db.once('open', () => { // 連線成功
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
