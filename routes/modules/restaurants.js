e2565d23c88b55672270364bf29263fae38970ff
sd880428 <sd660428@gmail.com>
2022-12-17 23:39:40 +0800

    合併router.create頁到restaurants update README

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -2,6 +2,15 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant') //載入restarunant models
 
+router.get('/create', (req, res) => {
+  res.render('create')
+})
+
+router.post('/create', (req, res) => {
+  return Restaurant.create({ ...req.body })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error)) //錯誤處理
+})
 router.get('/:restaurant', (req, res) => { //詳細資訊
   const id = req.params.restaurant //餐廳ID
   return Restaurant.findById(id) //從資料庫抓出與該ID相同的餐廳
@@ -19,13 +28,6 @@ router.get('/edit/:restaurant', (req, res) => { //編輯頁面
 })
 
 
-
-
-router.post('/create', (req, res) => {
-  return Restaurant.create({ ...req.body })
-    .then(() => res.redirect('/'))
-    .catch(error => console.error(error)) //錯誤處理
-})
 
 router.put('/:restaurant', (req, res) => { //編輯資訊
   const id = req.params.restaurant
